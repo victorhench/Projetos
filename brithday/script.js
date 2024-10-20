68727c637497bf9956e4e27e60371801d00b4edc
victorhench <bizarrevictor@gmail.com>
2024-10-20 16:32:14 -0300

    commit

--- brithday/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('open-btn').addEventListener('click', function() {
-    const gift = document.getElementById('gift');
-    const message = document.getElementById('message');
-    const button = document.getElementById('open-btn');
-
-    gift.style.display = 'none'; // Esconde o presente
-    button.style.display = 'none'; // Esconde o botão
-    message.classList.remove('hidden'); // Mostra a mensagem
-});
